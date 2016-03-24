cc10a4321fd23c8880604e09f97574dc198f6f2e
Sino- <SinisterSino@gmail.com>
2016-03-24 00:40:30 -0400

    Convert AM/PM Time to Military (24h) time

---
+++ TimeConversion.py
@@ -0,0 +1,26 @@
+#!/bin/python
+
+# Convert AM/PM Time to Military (24h) time
+# Input format ex: 07:05:45PM
+import sys
+
+time = raw_input().strip()
+
+def timeConverter(time):
+    hours = time[0:2] 
+    minutes = time[3:5]
+    seconds = time[6:8]
+    pam = time[8]
+    if (pam == 'P' or pam == 'p') and hours != "12":
+        hours = int(hours) + 12;
+    if (pam == 'A' or pam == 'a') and hours == "12":
+        hours = 0;
+    
+    if hours == 0:
+        hours = "0" + str(hours)
+    
+    return str(hours) + ":" + str(minutes) + ":" + str(seconds)
+
+print timeConverter(time)
+
+# Test Cases #0-#9: 0s
