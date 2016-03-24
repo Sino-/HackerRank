be912d9e86f45aebe97ec96042ebdaaed1042792
Sino- <SinisterSino@gmail.com>
2016-03-24 00:37:26 -0400

    Convert AM/PM Time to Military (24h) time

---
+++ Time-Conversion.java
@@ -0,0 +1,34 @@
+// Convert AM/PM Time to Military (24h) time
+// Input format ex: 07:05:45PM
+import java.util.*;
+
+public class TimeConversion {
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String input = kb.nextLine();
+        int seconds = Integer.parseInt(input.substring(6,8));
+        int minutes = Integer.parseInt(input.substring(3,5));
+        int hours = Integer.parseInt(input.substring(0,2));
+        char apm = input.charAt(8);
+		
+        if ((apm == 'P' || apm == 'p') && hours != 12)
+        	hours = hours + 12;
+        if ((apm == 'A' || apm == 'a') && hours == 12)
+        	hours = 0;
+        System.out.printf("%02d:%02d:%02d", hours, minutes, seconds);
+        kb.close();
+    }
+}
+/*
+Test Case #0:   0.09s 
+Test Case #1:   0.09s 
+Test Case #2:   0.08s 
+Test Case #3:   0.08s 
+Test Case #4:   0.09s 
+Test Case #5:   0.09s 
+Test Case #6:   0.09s 
+Test Case #7:   0.09s 
+Test Case #8:   0.09s 
+Test Case #9:   0.08s
+*/
