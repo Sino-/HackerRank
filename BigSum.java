447cea25af9d3f579b2c6ba0d4b071d647a9e7d4
Sino- <SinisterSino@gmail.com>
2016-03-26 10:22:46 -0400

    Return A Very Big Sum

---
+++ BigSum.java
@@ -0,0 +1,22 @@
+
+//https://www.hackerrank.com/challenges/a-very-big-sum
+import java.util.*;
+
+public class BigSum {
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        long ArraySize = kb.nextInt();
+        long sum = 0;
+        for (long i = 0 ; i < ArraySize ; i++)
+        {
+            sum+=kb.nextLong();
+        }
+        System.out.println(sum);
+    }
+}
+
+/*
+Test Case #0:   0.06s 
+Test Case #1:   0.07s
+*/
