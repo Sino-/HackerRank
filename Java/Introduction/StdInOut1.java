b977a4a666a610f40bd26d43647db0eeb46dab67
Sino- <SinisterSino@gmail.com>
2016-03-27 13:26:57 -0400

    Java Stdin and Stdout 1

---
+++ Java/Introduction/StdInOut1.java
@@ -0,0 +1,17 @@
+
+//https://www.hackerrank.com/challenges/java-stdin-and-stdout-1
+import java.util.*;
+
+public class StdInOut1 {
+
+    public static void main(String[] args) {
+      Scanner kb=new Scanner(System.in);   
+      System.out.println(kb.nextInt()+"\n"+kb.nextInt()+"\n"+kb.nextInt());
+    }
+}
+
+/*
+Test Case #0:   0.09s 
+Test Case #1:   0.09s 
+Test Case #2:   0.09s
+*/
