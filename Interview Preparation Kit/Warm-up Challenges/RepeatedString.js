b8fda70db28097fabe3e7555c8060d51c9864806
Sino Olabi <Sino-@users.noreply.github.com>
2018-12-14 15:08:12 -0800

    completed repeated string passing all test cases

---
+++ Interview Preparation Kit/Warm-up Challenges/RepeatedString.js
@@ -0,0 +1,40 @@
+//https://www.hackerrank.com/challenges/repeated-string/problem
+
+function repeatedString(s, n) {
+    let letterA = 0;
+    let i = s.length;
+    let nLessThanS = false;
+
+    if (n < i) {
+        i = n;
+        nLessThanS = true;
+    }
+
+    while (i--) {
+        if (s.charAt(i) === 'a') {
+            letterA++;
+        }
+    }
+    
+    //edge case if n is less than string length
+    if (nLessThanS) {
+        return letterA;
+    }
+
+    let remainingLetters = n % s.length - 1;
+    let remainder = 0;
+
+    if (remainingLetters >= 0) {
+        while (remainingLetters >= 0) {
+            if (s.charAt(remainingLetters) === 'a') {
+                remainder++;
+            }
+            remainingLetters--;
+        }
+    }
+
+    const multiplier = Math.floor(n / s.length);
+
+    return (letterA * multiplier + remainder);
+
+}
